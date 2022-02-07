91368bf4c7ea747b537cd53661f7c13135c9775f
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-02-07 20:48:17 +1000

    Added Tests

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -11,23 +11,4 @@ public class StatisticsService {
         }
         return current_max;
     }
-
-
-    public long findMin(long[] incomes) {
-        long current_max_index = 0;
-        long current_min = incomes[0];
-        for (long income : incomes){
-            if (current_min >= income)
-                current_min = income;}
-        return current_min;
-    }
-
-
-    public long findAverage(long[] incomes) {
-        long sum = 0;
-        for (long income : incomes){
-            sum = sum += income;
-            }
-        return sum / incomes.length;
-    }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,25 +19,50 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMin() {
+    void findMaximum() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
+        long[] incomesInBillions = {1200, 1215, 132138, 2134, 1235, 22313, 12312318, 1323126, 12312311, 123111, 123112};
+        long expected = 12312318;
 
-        long actual = service.findMin(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
     @Test
-    void findAverage() {
+    void findMax2() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 7;
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+        long expected = -3;
 
-        long actual = service.findAverage(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMax3() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1};
+        long expected = 1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMax4() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
+
 }
